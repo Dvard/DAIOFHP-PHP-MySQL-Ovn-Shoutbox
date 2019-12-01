c77e1637788bd2d0af31bb082ae149285666f884
Edvard DHG19 <56823476+Dvard@users.noreply.github.com>
2019-12-01 20:25:43 +0200

    Create db-connect.php

---
+++ db-connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username = "";
+$password = "";
+$db = "shoutbox";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
